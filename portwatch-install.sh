a56f01be8cd04eabc2a2a4bdbe4b6607d22d2c46
Alexander Faucher <alexanderfaucher+git@gmail.com>
2012-05-28 16:27:19 -0700

    hey, it works

---
+++ portwatch-install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+sudo apt-get install python-feedparser
+sudo xdg-icon-resource install --theme ubuntu-mono-dark --novendor --size 22 dark/indicator-portwatch.png indicator-portwatch
+sudo xdg-icon-resource install --theme ubuntu-mono-dark --novendor --size 22 dark/indicator-portwatch-attention.png indicator-portwatch-attention
+sudo xdg-icon-resource install --theme ubuntu-mono-light --novendor --size 22 light/indicator-portwatch.png indicator-portwatch
+sudo xdg-icon-resource install --theme ubuntu-mono-light --novendor --size 22 light/indicator-portwatch-attention.png indicator-portwatch-attention
+sudo xdg-icon-resource install --theme hicolor --novendor --size 22 hicolor/indicator-portwatch.png indicator-portwatch
+sudo xdg-icon-resource install --theme hicolor --novendor --size 22 hicolor/indicator-portwatch-attention.png indicator-portwatch-attention
+sudo xdg-icon-resource install --theme hicolor --novendor --size 128 --context apps portwatch-logo.png portwatch
+sudo xdg-icon-resource install --theme hicolor --novendor --size 48 --context apps portwatch-48x48.png portwatch
+
+sudo xdg-desktop-menu install --novendor portwatch.desktop
+
+if [ ! -d /usr/share/portwatch/ ]
+then
+sudo mkdir /usr/share/portwatch
+fi
+
+sudo cp portwatch-icon.png /usr/share/portwatch/portwatch-icon.png
+sudo cp portwatch-logo.png /usr/share/portwatch/portwatch-logo.png
+
+sudo cp portwatch /usr/bin/portwatch
+sudo chmod +x /usr/bin/portwatch
+
+MESSAGE="Feedindicator install completed..."
+echo $MESSAGE
+
---
+++ portwatch-uninstall.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+sudo xdg-icon-resource uninstall --theme ubuntu-mono-dark --size 22 indicator-portwatch
+sudo xdg-icon-resource uninstall --theme ubuntu-mono-dark --size 22 indicator-portwatch-attention
+sudo xdg-icon-resource uninstall --theme ubuntu-mono-light --size 22 indicator-portwatch
+sudo xdg-icon-resource uninstall --theme ubuntu-mono-light --size 22 indicator-portwatch-attention
+sudo xdg-icon-resource uninstall --theme hicolor --size 22 indicator-portwatch
+sudo xdg-icon-resource uninstall --theme hicolor --size 22 indicator-portwatch-attention
+sudo xdg-icon-resource uninstall --theme hicolor --size 128 --context apps portwatch
+sudo xdg-icon-resource uninstall --theme hicolor --size 48 --context apps portwatch
+
+sudo xdg-desktop-menu uninstall portwatch.desktop
+
+sudo unlink /usr/bin/portwatch
+
+sudo unlink /usr/share/portwatch/portwatch-icon.png
+sudo unlink /usr/share/portwatch/portwatch-logo.png
+
+sudo rmdir /usr/share/portwatch
+
+[ ! "$XDG_CACHE_HOME" ] && XDG_CACHE_HOME=~/.cache
+cachedir="$XDG_CACHE_HOME"/portwatch
+[ ! "$XDG_CONFIG_HOME" ] && XDG_CONFIG_HOME=~/.config
+configdir="$XDG_CONFIG_HOME"/portwatch
+autostartfile="$XDG_CONFIG_HOME"/autostart/portwatch.desktop
+
+if [ -f "$autostartfile" ]
+then
+unlink "$autostartfile"
+fi
+
+if [ -d "$configdir" ]
+then
+rm -r "$configdir"
+fi
+
+if [ -d "$cachedir" ]
+then
+rm -r "$cachedir"
+fi
+
+MESSAGE="Feedindicator uninstall completed..."
+echo $MESSAGE
+
